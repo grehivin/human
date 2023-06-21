22d1fae3d2c050ae20032d2482c67ddd80e7c3e2
grehivin <132721472+grehivin@users.noreply.github.com>
2023-06-20 22:38:12 -0600

    Delete TCU/Curso_DDHH_ESP directory

--- TCU/Curso_DDHH_ESP/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Curso_DDHH_ESP.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
